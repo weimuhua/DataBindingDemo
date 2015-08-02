2f7def2215825a9beca73199c8eafcb1638df845
weimuhua <weimuhua0812@126.com>
2015-08-02 21:03:44 +0800

    add button onClick even.

--- app/src/main/java/mh/com/databindingdemo/User.java
+++ app/src/main/java/mh/com/databindingdemo/User.java
@@ -1,7 +1,13 @@
 package mh.com.databindingdemo;
 
 
+import android.util.Log;
+import android.view.View;
+
 public class User {
+
+    private static final String TAG = "User";
+
     private String firstName;
     private String lastName;
 
@@ -16,5 +22,19 @@ public class User {
 
     public String getLastName() {
         return lastName;
+    }
+
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.first_name:
+                Log.d(TAG, "first_name onClick");
+                break;
+            case R.id.last_name:
+                Log.d(TAG, "last_name onClick");
+                break;
+            default:
+                break;
+        }
     }
 }
